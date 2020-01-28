5fc22208fc904732735fef1e5e01f2a5021536f6
Kim-SuBin <46712693+Kim-SuBin@users.noreply.github.com>
2020-01-28 17:17:02 +0900

    Update main.py
    주차 가능한 자동차 수 표시 및 자동차 입차, 출차 시간 입력 테이블 생성

--- JetsonNano/imageProcessing/main.py
+++ JetsonNano/imageProcessing/main.py
@@ -1,6 +1,8 @@
 import keyboard
 import numpy as np
 import cv2
+import time
+import datetime
 
 img = cv2.imread("img/parkingLot.png", cv2.IMREAD_COLOR)
 
@@ -13,6 +15,10 @@ parkingList = [(167, 375), (221, 375), (275, 375), (329, 375), (383, 375),
 # green = cv2.circle(img, parkingList[0], 7, (0, 255, 0), -1)
 # red = cv2.circle(img, parkingList[0], 7, (0, 0, 255), -1)
 
+carTable = [[0 for _ in range(4)] for _ in range(30)] # 차량 번호, 입차 시간, 출차 시간, 주차 구역
+index = 0
+count = 22
+
 for i in range(len(parkingList)):
     cv2.circle(img, parkingList[i], 7, (0, 255, 0), -1)
 
@@ -22,25 +28,28 @@ while 1:
     if k == 27:  # esc key
         break
     elif k == ord('i'):
-        key = int(input("press key:"))
-        cv2.circle(img, parkingList[int(key) - 1], 7, (0, 0, 255), -1)
+        print("주차 가능 :", count, " /22")
+        count -= 1
+        carTable[index][0] = input("press incar: ")
+        # 들어간 시간 입력
+        now = datetime.datetime.now()
+        carTable[index][1] = now.strftime('%Y-%m-%d %H:%M:%S')
+        key = int(input("press key:")) # 구역
+        # 구역 입력
+        carTable[index][3] = key
+        cv2.circle(img, parkingList[int(key) - 1], 7, (0, 0, 255), -1) # 색변경
+        index += 1
     elif k == ord('o'):
+        count += 1
+        car = input("press outcar: ")
+        for i in range(len(carTable)):
+            if car == carTable[i][0]: # 나간 시간 입력
+                now = datetime.datetime.now()
+                carTable[i][2] = now.strftime('%Y-%m-%d %H:%M:%S')
         key = int(input("press key:"))
-        cv2.circle(img, parkingList[int(key) - 1], 7, (0, 255,0), -1)
-
-
-    # key = keyboard.read_key()
-
-    # if key == 'esc':
-    #     #     break
-    # elif key:
-    #     cv2.circle(img, parkingList[int(key)-1], 7, (0, 0, 255), -1)
-    #     a = int(input())
-    #     cv2.circle(img, parkingList[a-1], 7, (0, 0, 255), -1)
-    #     if keyboard.is_pressed("1"):
-    #         cv2.circle(img, parkingList[a-1], 7, (0, 0, 255), -1)
-    #     elif keyboard.is_pressed("1"):
-    #         cv2.circle(img, parkingList[a-1], 7, (0, 0, 255), -1)
+        cv2.circle(img, parkingList[int(key) - 1], 7, (0, 255,0), -1) # 색변경
+        print("주차 가능 :", count, " /22")
+        print(carTable)
 
 
 cv2.destroyAllWindows()
